152c07ccf3048239368ca783c7482a9de9f38cbf
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-21 14:51:59 -0500

    Added post for /api/register to index.js and renamed db

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const bcrypt = require("bcryptjs"); // Added package and required it here
 
 const db = require("./database/dbConfig.js");
 
@@ -10,11 +11,25 @@ const server = express();
 server.use(helmet());
 server.use(morgan("short"));
 server.use(express.json());
+server.use(cors());
 
-server.get("/", (req, res) => {
-  res.send("Keep coding, it's Working");
+server.post("/api/register", (req, res) => {
+  // Grab the username and password from body
+  const creds = req.body;
+  // generate the hash from the user's password
+  const hash = bcrypt.hashSync(creds.password, 14); // rounds is 2^X
+  // override the user.password with the hash
+  creds.password = hash;
+  // save the user to the database
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
 });
 
+// Protect this route, only authenticated users should see it
 server.get("/api/users", (req, res) => {
   db("users")
     .select("id", "username")
